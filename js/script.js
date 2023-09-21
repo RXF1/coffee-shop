08232804aacf9ccf8e8af8161082e773c6139cd7
Ahmadrafii <rafia0020@gmail.com>
2023-09-21 15:47:13 +0700

    seachbox

--- js/script.js
+++ js/script.js
@@ -4,13 +4,26 @@ const navbarNav= document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick=()=>{
     navbarNav.classList.toggle('active');
 };
+// toggle kelas active search
+const seacrhForm = document.querySelector('.search-form');
+const searchBox =document.querySelector('#search-box');
 
+document.querySelector('#search-button').onclick=(e)=>{
+    seacrhForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
 //klik diluar hamburger
 const hamburger=document.querySelector('#hamburger-menu');
-
+const sb=document.querySelector('#search-button');
 document.addEventListener('click', function(e){
     if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
+    }
+})
+document.addEventListener('click', function(e){
+    if(!sb.contains(e.target)&& !seacrhForm.contains(e.target)){
+        seacrhForm.classList.remove('active');
     }
 })
 
