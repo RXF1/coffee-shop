6963016b6ba832fa082055039c9d58e20d74135c
Ahmadrafii <rafia0020@gmail.com>
2023-09-25 08:09:09 +0700

    <selsai>

--- js/script.js
+++ js/script.js
@@ -13,9 +13,18 @@ document.querySelector('#search-button').onclick=(e)=>{
     searchBox.focus();
     e.preventDefault();
 }
+// shoppingcart
+const sc=document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick=(e)=>{
+    sc.classList.toggle('active');
+    e.preventDefault();
+};
 //klik diluar hamburger
 const hamburger=document.querySelector('#hamburger-menu');
 const sb=document.querySelector('#search-button');
+const scb=document.querySelector('#shopping-cart-button');
+
 document.addEventListener('click', function(e){
     if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
@@ -26,4 +35,35 @@ document.addEventListener('click', function(e){
         seacrhForm.classList.remove('active');
     }
 })
+
+document.addEventListener('click', function(e){
+    if(!scb.contains(e.target)&& !sc.contains(e.target)){
+        sc.classList.remove('active');
+    }
+})
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailbutton= document.querySelectorAll('.item-detail-button')
+
+itemDetailbutton.forEach((btn) => {
+    btn.onclick = (e) =>{
+    itemDetailModal.style.display ='flex';
+    e.preventDefault();
+    };
+});
+
+// click tombol close
+document.querySelector('.modal .close-icon').onclick = (e) =>{
+    itemDetailModal.style.display= 'none';
+    e.preventDefault();
+}
+
+// click diluar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+}
+
 
